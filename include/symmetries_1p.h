9840af45d8a0b9409180ec295c4413b2cba90291
Nils Wentzell <nils.wentzell@gmail.com>
2015-08-31 21:36:10 +0200

    .. new container seems to be working

--- include/symmetries_1p.h
+++ include/symmetries_1p.h
@@ -16,24 +16,24 @@
 #include <symmap_1p.h>
 #include <const.h>
 
-typedef operation ( *symm_func_1p_t )( index_1p_t& ); ///< Symmetry function that acts on an index_1p_t object and alters it 
+typedef operation ( *symm_func_1p_t )( idx_1p_t& ); ///< Symmetry function that acts on an idx_1p_t object and alters it 
 
 /**
  *	Apply symmetries and establish mapping	
  */
-void init_symm( std::shared_ptr<se_tensor> se_ptr, std::vector<index_1p_t>& ind_cpl_list ); 
+void init_symm( std::shared_ptr<se_tensor> se_ptr, std::vector<idx_1p_t>& ind_cpl_list ); 
 /**
  *	Iterate a symmetry on vertex object	
  */
-void iterate( const index_1p_t& ind, const operation& track_op, se_tensor& vertex, se_checked_tensor& checked, std::vector<symm_func_1p_t> symm_func_list, int ind_cpl_list_pos );
+void iterate( const idx_1p_t& idx, const operation& track_op, se_tensor& vertex, se_checked_tensor& checked, std::vector<symm_func_1p_t> symm_func_list, int ind_cpl_list_pos );
 
 // Symmetries
-operation compl_conj( index_1p_t& ind );		///< Complex conjugation 
-operation time_rev( index_1p_t& ind );		///< Time reversal symmetry
-operation particle_hole( index_1p_t& ind );	///< Particle hole symmetry
-operation spin_symm( index_1p_t& ind );		///< Spin symmetry in nambu notation
+operation compl_conj( idx_1p_t& idx );		///< Complex conjugation 
+operation time_rev( idx_1p_t& idx );		///< Time reversal symmetry
+operation particle_hole( idx_1p_t& idx );	///< Particle hole symmetry
+operation spin_symm( idx_1p_t& idx );		///< Spin symmetry in nambu notation
 
-operation rot_k( index_1p_t& ind );		///< Rotate all momenta by 90 degrees
-operation mirror_vert( index_1p_t& ind );		///< Mirror all momenta vertically
+operation rot_k( idx_1p_t& idx );		///< Rotate all momenta by 90 degrees
+operation mirror_vert( idx_1p_t& idx );		///< Mirror all momenta vertically
 
 #endif 
--- src/symmetries_1p.cpp
+++ src/symmetries_1p.cpp
@@ -8,10 +8,11 @@
 
 
 #include <symmetries_1p.h>
+#include <def.h>
 
 using namespace std;
 
-void init_symm( shared_ptr<se_tensor> se_ptr, vector<index_1p_t>& ind_cpl_list )
+void init_symm( shared_ptr<se_tensor> se_ptr, vector<idx_1p_t>& ind_cpl_list )
 {
    // Initialize vector containing all symmetry functions 
    vector<symm_func_1p_t> symm_func_list; // = { exch_in, exch_out, compl_conj, time_rev, particle_hole, rot_k, mirror_vert, mirror_diag };
@@ -33,86 +34,86 @@ void init_symm( shared_ptr<se_tensor> se_ptr, vector<index_1p_t>& ind_cpl_list )
 	 for( int s_in = 0; s_in < QN_COUNT; ++s_in )
 	    for( int s_out = 0; s_out < QN_COUNT; ++s_out )
 	    {
-	       index_1p_t ind( w, k, s_in, s_out );
-	       if ( !checked( ind ) )  						// if tensor object not yet related to any other
+	       idx_1p_t idx( w, k, s_in, s_out );
+	       if ( !checked( idx ) )  						// if tensor object not yet related to any other
 	       {
-		  checked( ind ) = true; 
-		  ind_cpl_list.push_back( ind );				// push standard representative into ind_cpl_list
+		  checked( idx ) = true; 
+		  ind_cpl_list.push_back( idx );				// push standard representative into ind_cpl_list
 		  int ind_cpl_list_pos =  ind_cpl_list.size() - 1 ; 		// position in ind_cpl_list is size - 1
-		  (*se_ptr )( ind ) = ind_cpl_t( ind_cpl_list_pos ); 		// save position with trivial operations
+		  (*se_ptr )( idx ) = ind_cpl_t( ind_cpl_list_pos ); 		// save position with trivial operations
 		  operation track_op( false, false );
-		  iterate( ind, track_op, *se_ptr, checked, symm_func_list, ind_cpl_list_pos ); 		// start iterating on index 
+		  iterate( idx, track_op, *se_ptr, checked, symm_func_list, ind_cpl_list_pos ); 		// start iterating on index 
 	       }
 	    }
 
 
 }
 
-void iterate( const index_1p_t& ind, const operation& track_op, se_tensor& selfEn, se_checked_tensor& checked, vector<symm_func_1p_t> symm_func_list, int ind_cpl_list_pos )
+void iterate( const idx_1p_t& idx, const operation& track_op, se_tensor& selfEn, se_checked_tensor& checked, vector<symm_func_1p_t> symm_func_list, int ind_cpl_list_pos )
 {
    for( auto symm_func: symm_func_list ) 				// iterate over list of all symmetries specified
    {
-      index_1p_t ind_it = ind;						// copy ind
-      operation curr_op = symm_func( ind_it ) * track_op;		// apply symmetry operation and track operations applied
-      if( !checked( ind_it ) )						// if resulting tensor index not yet related to any other
+      idx_1p_t idx_it = idx;						// copy ind
+      operation curr_op = symm_func( idx_it ) * track_op;		// apply symmetry operation and track operations applied
+      if( !checked( idx_it ) )						// if resulting tensor index not yet related to any other
       { 
-	 checked( ind_it ) = true; 
-	 selfEn( ind_it ) = ind_cpl_t( ind_cpl_list_pos, curr_op ); 	// relate to position
-	 iterate( ind_it, curr_op, selfEn, checked, symm_func_list, ind_cpl_list_pos );	// iterate further	
+	 checked( idx_it ) = true; 
+	 selfEn( idx_it ) = ind_cpl_t( ind_cpl_list_pos, curr_op ); 	// relate to position
+	 iterate( idx_it, curr_op, selfEn, checked, symm_func_list, ind_cpl_list_pos );	// iterate further	
       }
    }
 }
 
 //----- Antisymmetry
 
-operation compl_conj( index_1p_t& ind )
+operation compl_conj( idx_1p_t& idx )
 {
-   freq_sign_change( ind.w );
+   freq_sign_change( idx( w ) );
 
    // Changing momenta sign is unnecessary since equal to rotating twice by 90 degrees
 
-   swap( ind.s_in, ind.s_out );
+   swap( idx( s_in ), idx( s_out ) );
    return operation( false, true );
 }
 
-operation time_rev( index_1p_t& ind )
+operation time_rev( idx_1p_t& idx )
 {
-   swap( ind.s_in, ind.s_out );
+   swap( idx( s_in ), idx( s_out ) );
    return operation( false, false );
 }
 
-operation particle_hole( index_1p_t& ind )
+operation particle_hole( idx_1p_t& idx )
 {
 #ifndef NO_MOMENTA
-   mirror_mom_pipi( ind.k );
+   mirror_mom_pipi( idx( k ) );
 #endif
    return operation( false, true );
 }
 
-operation spin_symm( index_1p_t& ind )
+operation spin_symm( idx_1p_t& idx )
 {
-   freq_sign_change( ind.w );
+   freq_sign_change( idx( w ) );
    
-   swap( ind.s_in, ind.s_out );
+   swap( idx( s_in ), idx( s_out ) );
 
-   flip_spin( ind.s_in );
-   flip_spin( ind.s_out );
+   flip_spin( idx( s_in ) );
+   flip_spin( idx( s_out ) );
 
-   if ( ( ind.s_in + !( ind.s_out ) ) % 2  != 0 ) // if uneven amount of creation operators minus sign, see masterthesis
+   if ( ( idx( s_in ) + !( idx( s_out ) ) ) % 2  != 0 ) // if uneven amount of creation operators minus sign, see masterthesis
       return operation( true, false );
 
    return operation( false, false );
 }
 
-operation rot_k( index_1p_t& ind )
+operation rot_k( idx_1p_t& idx )
 {
-   ind.k = rot_k_ind_arr[ind.k];
+   idx( k ) = rot_k_idx_arr[idx( k )];
    return operation( false, false );
 }
 
-operation mirror_vert( index_1p_t& ind )
+operation mirror_vert( idx_1p_t& idx )
 {
-   mirror_mom_vert( ind.k );
+   mirror_mom_vert( idx( k ) );
    return operation( false, false );
 }
 
